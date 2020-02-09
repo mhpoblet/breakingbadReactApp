872b122776b0d9a540e3a5a6d0ff3d721e41cdf3
Michaela Poblete <mich@campus-040-211.ucdavis.edu>
2020-02-08 18:27:03 -0800

    parsing Vision correctly now

--- at2/index.js
+++ at2/index.js
@@ -1,17 +1,35 @@
+var http = require("https");
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
 async function quickstart() {
   // Imports the Google Cloud client library
   const vision = require('@google-cloud/vision');
 
   // Creates a client
   const client = new vision.ImageAnnotatorClient();
-
+ 
   // Performs label detection on the image file
   const [result] = await client.labelDetection('./resources/cookie.jpg');
   const labels = result.labelAnnotations;
   console.log('Labels:');
-  console.log("/");
-  labels.forEach(label => console.log(label.description));
+  
+
+  //labels.forEach(label => console.log(label.description));
+  
+  var now = labels.find(label => label.description == "Cookie");
+
+  
+  console.log(now.description);
+  app.get('/', (req, res) => res.send(now));
 }
+
+
 // [END vision_quickstart]
 
 quickstart().catch(console.error);
--- at2/nutrition.js
+++ at2/nutrition.js
@@ -4,7 +4,7 @@ var options = {
 	"method": "GET",
 	"hostname": "edamam-edamam-nutrition-analysis.p.rapidapi.com",
 	"port": null,
-	"path": "/api/nutrition-data?ingr=cookie",
+	"path": "/api/nutrition-data?ingr=oreo",
 	"headers": {
 		"x-rapidapi-host": "edamam-edamam-nutrition-analysis.p.rapidapi.com",
 		"x-rapidapi-key": "db0ac482b7msha6f292ac5a296b8p198b2fjsn6f65ec39bc2f"
@@ -19,14 +19,9 @@ var req = http.request(options, function (res) {
 	});
 
 	res.on("end", function () {
-        var body = Buffer.concat(chunks);
-        var specs = JSON.parse(body);
-        if (specs.calories>=0) {
-            console.log(specs.calories);
-        }
-		//console.log(body.toString());
+		var body = Buffer.concat(chunks);
+		console.log(body.toString());
 	});
 });
 
 req.end();
-
